45f68536c0e57248cfe922bdc5a454077deb8b76
perry1224 <perry.w1224@gmail.com>
2022-07-27 14:12:22 -0400

    Updates

---
+++ precache-manifest.31688622b201d4e33eb424473a22bbdd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "811ba7a201a7e2013d75",
+    "url": "/meet/static/css/main.a3e1201e.chunk.css"
+  },
+  {
+    "revision": "811ba7a201a7e2013d75",
+    "url": "/meet/static/js/main.811ba7a2.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/meet/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/meet/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "a21e384eb6843b2b897d",
+    "url": "/meet/static/js/runtime~main.a21e384e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/meet/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "beca54c5855e64e52735fa70c4db5c18",
+    "url": "/meet/index.html"
+  }
+];
