08a59215034009f8f9bf008a20ac48bdd00b8137
James Katarikawe <jpkatarikawe@gmail.com>
2022-08-09 17:00:53 -0500

    fix runtime errors

--- src/App.js
+++ src/App.js
@@ -1,69 +1,63 @@
-import React, { Component } from 'react';
-import EventList from './EventList';
-import './App.css';
-import CitySearch from './CitySearch';
-import NumberofEvents from './NumberOfEvents';
-import { getEvents, extractLocations } from './api';
-import './nprogress.css';
+import React, { Component } from "react";
+import EventList from "./EventList";
+import "./App.css";
+import CitySearch from "./CitySearch";
+import NumberofEvents from "./NumberOfEvents";
+import { getEvents, extractLocations } from "./api";
+import "./nprogress.css";
 
-
-class App extends Component  {
-  state = {
-    events: [],
-    locations: []
-  };
-
-
-  render () {
-    console.log(this, this.updateEvents)
-    return (
-      <div className="App">
-      <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
-      <NumberofEvents updateEvents={this.updateEvents} />
-      <EventList events = {this.state.events} />
-  
-      
-      
-      </div>
-    );
-  }
+class App extends Component {
   state = {
     events: [],
     locations: [],
     numberOfEvents: 32,
-    activeLocation: 'all',
+    activeLocation: "all",
+  };
+
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
   }
 
-updateEvents = (location, eventCount = 
-    this.state.eventCount) => {
-     getEvents().then((events) => {
-      let locationEvents = (location === "all" ? 
-         events : events.filter((event) => event.location === location));
-      locationEvents = locationEvents.slice(0, eventCount)
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
+  updateEvents = (location, eventCount = this.state.eventCount) => {
+    getEvents().then((events) => {
+      let locationEvents =
+        location === "all"
+          ? events
+          : events.filter((event) => event.location === location);
+      locationEvents = locationEvents.slice(0, eventCount);
       this.setState({
         events: locationEvents,
         numberOfEvents: eventCount,
-        activeLocation: location
+        activeLocation: location,
       });
     });
-    
+  };
+
+  updateNumberOfEvents = (eventCount) => {
+        this.updateEvents(this.state.activeLocation, eventCount);
   }
 
-  // updateNumberOfEvents = (eventCount) => {
-  //        updateEvents(this.state.activeLocation, eventCount);
-
-componentDidMount() {
-  this.mounted = true;
-  getEvents().then((events) => {
-    if (this.mounted) {
-      this.setState({ events, locations: extractLocations(events) });
-    }
-  });
-}
-
-componentWillUnmount(){
-  this.mounted = false;
-}
+  render() {
+    return (
+      <div className="App">
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
+        />
+        <NumberofEvents updateEvents={this.updateNumberOfEvents} />
+        <EventList events={this.state.events} />
+      </div>
+    );
+  }
 }
 
 export default App;
