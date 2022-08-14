922bad83d172c7158788f96a33f4bbdfe326d9fa
perry1224 <perry.w1224@gmail.com>
2022-08-14 19:58:03 -0400

    updated index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+
+atatus.config('cec4fcbe047241fcb932606ae34c98fb').install();
 
 const root = document.getElementById("root");
 render(<App />, root);
@@ -17,3 +20,4 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+atatus.notify(new Error('Test Atatus Setup'));
