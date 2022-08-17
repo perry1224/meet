8ea2cd254fdac698c1a44d162a3c8c5895cd42ca
perry1224 <perry.w1224@gmail.com>
2022-08-17 18:46:07 -0400

    updated api.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import * as atatus from 'atatus-spa';
 
 
 
-
 const root = document.getElementById("root");
 render(<App />, root);
 
@@ -16,6 +15,7 @@ render(<App />, root);
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
