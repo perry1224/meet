6c9868481cae38834e5cf83ecaeba573f5051157
perry1224 <perry.w1224@gmail.com>
2022-08-27 15:41:12 -0400

    Update EventGenre.js

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -1,45 +1,46 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState, useEffect } from "react";
+
 import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
 
 const EventGenre = ({ events }) => {
   const [data, setData] = useState([]);
-
   const colors = ['#e600e6', '#0000b3', '#00802b', '#FF8042', '#6a0dad'];
-  const getData = () => {
-    const genres = ["React", "JavaScript", "Node", "jQuery", "AngularJS"];
 
-    const data = genres.map((genre) => {
-      const value = events.filter((event) =>
-        event.summary.split(" ").includes(genre)).length;
-      return {
-        name: genre,
-        value: value,
-      };
-    });
-    return data;
-  };
-
-  useEffect(() => {
+   useEffect(() => {
     setData(() => getData());
-  }, [events]);
+  }, [events]); 
+
+
+  
+const getData = () => {
+  const genres = ['React', 'Angular', 'JavaScript', 'Node','jQuery'];
+
+  const data = genres.map((genre) => {
+    const value = events.filter((event) =>
+    event.summary.includes(genre)).length;
+    return { name: genre, value };
+  });
+  return data;
+}
+
+
 
   return (
-    <ResponsiveContainer height={400}>
-      <PieChart width={400} height={400}>
+    <ResponsiveContainer height={400} width={'100%'}>
+      <PieChart height={400} width={400}>
         <Pie
           data={data}
           cx={200}
           cy={200}
           labelLine={false}
-          label={({ name, percent }) => {
-            if (percent > 0) {
-              return  `${name} ${(percent * 100).toFixed(0)}%`
-            }
-          }
-          }
           outerRadius={80}
           fill="#8884d8"
           dataKey="value"
+          label={({ name, percent }) => {
+            if (percent > 0) {
+              return `${name} ${(percent * 100).toFixed(0)}%`;
+            }
+          }}
         >
           {data.map((entry, index) => (
             <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
